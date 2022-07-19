17e5ca676b9b41bc86151f2d7d02d63660a72de6
Saúl Hiram Castillo Castro <csaulhiram@outlook.com>
2022-07-18 23:34:52 -0500

    CRUD completed

--- src/main/java/datos/PersonaDAO.java
+++ src/main/java/datos/PersonaDAO.java
@@ -1,5 +1,6 @@
 package datos;
 
+import static datos.Conexion.close;
 import static datos.Conexion.getConnection;
 import domain.Persona;
 import java.sql.*;
@@ -8,6 +9,9 @@ import java.util.*;
 public class PersonaDAO {
 
     private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM  persona";
+    private static final String SQL_INSERT = "INSERT INTO persona(nombre, apellido, email, telefono) VALUES(?, ?, ?, ?)";
+    private static final String SQL_DELETE = "DELETE  FROM persona WHERE id_persona = ?";
+    private static final String SQL_UPDATE = "UPDATE  persona SET nombre = ?, apellido = ?, email = ?, telefono = ? WHERE id_persona = ?";
 
     public List<Persona> seleccionar() {
 
@@ -48,5 +52,95 @@ public class PersonaDAO {
         }
         System.out.println(personas);
         return personas;
+    }
+
+    public int insertar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            // The first parameter corresponds to the firs value in our SQL query
+            //  VALUES(first, second, thirt, fourth)
+            stmt.setString(1, persona.getNombre());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getEmail());
+            stmt.setString(4, persona.getTelefono());
+
+            // Execute Query
+            registros = stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {// Close all connections
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return registros;
+    }
+
+    public void eliminar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_DELETE);
+
+            // The first parameter corresponds to the firs value in our SQL query
+            //  idpersona = first
+            stmt.setInt(1, persona.getIdPersona());
+            System.out.println(stmt);
+            // Execute Query
+            stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {// Close all connections
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public int actualizar(Persona persona) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0;
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            // The first parameter corresponds to the firs value in our SQL query
+            //  SET nombre = first, apellido = second, email = thirt, telefono = fourth WHERE id_persona = fifth
+            stmt.setString(1, persona.getNombre());
+            stmt.setString(2, persona.getApellido());
+            stmt.setString(3, persona.getEmail());
+            stmt.setString(4, persona.getTelefono());
+            stmt.setInt(5, persona.getIdPersona());
+
+            // Execute Query
+            registros = stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {// Close all connections
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return registros;
     }
 }
--- src/main/java/test/TestManejoPersonas.java
+++ src/main/java/test/TestManejoPersonas.java
@@ -4,14 +4,56 @@ import datos.PersonaDAO;
 import domain.Persona;
 import java.util.List;
 
-
 public class TestManejoPersonas {
+
     public static void main(String[] args) {
+
+        System.out.println("***************************************");
+        System.out.println("\t\tSELECT QUERY");
+        System.out.println("***************************************");
         PersonaDAO personaDao = new PersonaDAO();
         List<Persona> personas = personaDao.seleccionar();
-        
+
         // Show database information
-        for(Persona persona: personas) {
+        listarPersonas(personas);
+
+        System.out.println("***************************************");
+        System.out.println("\t\tINSERT QUERY");
+        System.out.println("***************************************");
+
+        // Creating object type Persona
+        Persona persona2 = new Persona("Peter", "Juarez", "test4@test.com", "1234567890");
+        // NOTE: the variable personaDAO was created at the last class 
+        personaDao.insertar(persona2);
+
+        // Show the new list of personas
+        personas = personaDao.seleccionar();
+        listarPersonas(personas);
+        System.out.println("***************************************");
+        System.out.println("\t\tDELETE QUERY");
+        System.out.println("***************************************");
+
+        Persona persona3 = new Persona(17);
+        personaDao.eliminar(persona3);
+
+        // Show the new list of personas
+        personas = personaDao.seleccionar();
+        listarPersonas(personas);
+
+        System.out.println("***************************************");
+        System.out.println("\t\tUPDATE QUERY");
+        System.out.println("***************************************");
+
+        Persona persona4 = new Persona(16, "saul hiram", "castilloCastro", "update@test.com", "77777777777");
+        personaDao.actualizar(persona4);
+
+        // Show the new list of personas
+        personas = personaDao.seleccionar();
+        listarPersonas(personas);
+    }
+
+    public static void listarPersonas(List<Persona> personas) {
+        for (Persona persona : personas) {
             System.out.println("persona: " + persona);
         }
     }
